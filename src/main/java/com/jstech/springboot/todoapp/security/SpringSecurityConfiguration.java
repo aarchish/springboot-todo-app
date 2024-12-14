94707ce0093b37638179dcbb148e907e8dc62050
Aarchish Jindal <aarchish0@gmail.com>
2024-12-14 17:57:31 +0530

    Spring Security Configuration for In-Memory H2 DB

--- src/main/java/com/jstech/springboot/todoapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/jstech/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,17 @@ package com.jstech.springboot.todoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -44,5 +48,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // by default Spring Security protects all HTTP requests
+    // To use H2 In-Memory DB, we need to disable CSRF (Cross-Site Request Forgery) and enable HTML Frames
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        
+
+        return http.build();
     }
 }
